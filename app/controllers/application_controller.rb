8096dff3b302b597061ad8249285792f0e9a77c9
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-30 17:51:29 +0100

    Generated user, artist and collector model. Updated sign up forms for both.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :account_type, account_attributes: [ :location, :nationality, :date_of_birth, :vita, :artschool ]])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, account_attributes:  [ :location, :nationality, :date_of_birth, :vita, :artschool ]])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :account, polymorphic: true
+
+  accepts_nested_attributes_for :account
+
+  def build_account(params)
+    self.account = account_type.constantize.new(params)
+  end
+
+  def collector?
+    account_type == "Collector"
+  end
+
+  def artist?
+    account_type == "Artist"
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
