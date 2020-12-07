c85baed7e3fbdc234be68a44b85897b5e58faece
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-12-07 12:16:25 +0100

    Updated seed with mediums and categories and fixed artwork add/edit

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,5 @@
 class Medium < ApplicationRecord
   has_many :artworks
 
-  validates :name, inclusion: { in: ['Oil paint', 'Watercolour', 'Pencil', 'Etching', 'Lithography', 'Bronze', 'Marble', 'Wood', 'Iron', 'Plastic', 'Acrylic paint', 'Chalk', 'Pastel'] }
+  validates :name, uniqueness: true, inclusion: { in: ['Oil paint', 'Watercolour', 'Pencil', 'Etching', 'Lithography', 'Bronze', 'Marble', 'Wood', 'Iron', 'Plastic', 'Acrylic paint', 'Chalk', 'Pastel'] }
 end
